372b486237c1a9fda1d0f111408c48c81678f03d
davidclaveau <daveclaveau@gmail.com>
2021-06-21 20:20:04 +0000

    Create first record for Burnaby store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,14 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true,
+  created_at: Time.now(),
+)
