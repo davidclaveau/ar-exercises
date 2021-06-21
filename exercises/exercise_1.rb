918a92bd086e5b1d44348476a588d8ef2039c584
davidclaveau <daveclaveau@gmail.com>
2021-06-21 13:27:05 -0700

    Find stores and update store id 1's name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# Richmond (annual_revenue of 1260000 carries women's apparel only)
-# Gastown (annual_revenue of 190000 carries men's apparel only)
 
 burnaby = Store.create(
   name: "Burnaby",
