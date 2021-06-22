27b69cc332f5f2ec8dc5feba299f3b18370d2adb
davidclaveau <daveclaveau@gmail.com>
2021-06-21 20:53:04 -0700

    Add relationship between tables and add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jerry", last_name: "Seinfeld", hourly_rate: 80)
+@store1.employees.create(first_name: "George", last_name: "Costanza", hourly_rate: 50)
+@store1.employees.create(first_name: "Elaine", last_name: "Benice", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Cosmo", last_name: "Kramer", hourly_rate: 70)
+@store2.employees.create(first_name: "Homer", last_name: "Simpson", hourly_rate: 70)
+@store2.employees.create(first_name: "Hank", last_name: "Hill", hourly_rate: 100)
+@store2.employees.create(first_name: "Boy", last_name: "Hill", hourly_rate: 40)
+
